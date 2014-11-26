9be136bf63f89295c9657adf888bd19b6e762389
Tom Koptel <tom.koptel@gmail.com>
2014-11-27 00:07:36 +0200

    Adding argument captor

--- plugin/src/main/groovy/com/rain/utils/android/robocop/RobocopPlugin.groovy
+++ plugin/src/main/groovy/com/rain/utils/android/robocop/RobocopPlugin.groovy
@@ -20,18 +20,24 @@ public class RoboCopPlugin implements Plugin<Project> {
             throw new ProjectConfigurationException("The android or android-library plugin must be applied to the project", null)
         }
 
+//        def robocopConfiguration = project.configurations.compile
+//        def robocopTestConfiguration = project.configurations.androidTestCompile
+
         project.extensions.create("robocop", RoboCopPluginExtension)
 
         project.afterEvaluate {
             project.android[variants].all { variant ->
                 configureVariant(project, variant)
+//                if (variant.testVariant) {
+//                    configureVariant(project, variant.testVariant, robocopTestConfiguration)
+//                }
             }
         }
     }
 
     static void configureVariant(def project, def variant) {
         if (project.robocop.schemaPath == null) {
-            project.getLogger().info('RoboCop: Schema path is not defined exiting')
+            project.getLogger().info('RoboCop: Schema path is not defined. Exiting!')
             return;
         }
         def schemaFile = new File(project.robocop.schemaPath)
@@ -39,16 +45,28 @@ public class RoboCopPlugin implements Plugin<Project> {
             throw new ProjectConfigurationException("Schema file path points to wrong file location", null)
         }
 
-        def aptOutputDir = project.file(new File(project.buildDir, "generated/source/db"))
-        def aptOutput = new File(aptOutputDir, variant.dirName)
+        def robocopOutputDir = project.file(new File(project.buildDir, "generated/source/db"))
+        def robocopOutput = new File(robocopOutputDir, variant.dirName)
 
-        variant.addJavaSourceFoldersToModel(aptOutput);
+        variant.addJavaSourceFoldersToModel(robocopOutput);
+
+//        def processorPath = configuration.getAsPath()
+//        variant.javaCompile.options.compilerArgs += [
+//                '-processorpath', processorPath,
+//                '-s', robocopOutput
+//        ]
+
+//        project.robocop.robocopArguments.variant = variant
+//        project.robocop.robocopArguments.project = project
+//        project.robocop.robocopArguments.android = project.android
+//
+//        variant.javaCompile.options.compilerArgs += project.robocop.arguments()
 
         def schemaPath = schemaFile.getAbsolutePath()
-        def generatedSourcePath = aptOutput.canonicalPath
+        def generatedSourcePath = robocopOutput.canonicalPath
 
         variant.javaCompile.doFirst {
-            aptOutput.mkdirs()
+            robocopOutput.mkdirs()
             ContentProviderGenerator.generateContentProvider(schemaPath, generatedSourcePath)
         }
     }
--- plugin/src/main/groovy/com/rain/utils/android/robocop/RobocopPluginExtension.groovy
+++ plugin/src/main/groovy/com/rain/utils/android/robocop/RobocopPluginExtension.groovy
@@ -5,4 +5,21 @@ package com.rain.utils.android.robocop
  */
 class RoboCopPluginExtension {
     String schemaPath
+
+//    final def robocopArguments = new RoboCopArguments()
+//    private def argsClosure;
+//
+//    def arguments(Closure closure) {
+//        closure.resolveStrategy = Closure.DELEGATE_FIRST
+//        closure.delegate = robocopArguments;
+//        argsClosure = closure;
+//    }
+//
+//    def arguments() {
+//        robocopArguments.arguments.clear();
+//        if (argsClosure) {
+//            argsClosure()
+//        }
+//        return robocopArguments.arguments
+//    }
 }
