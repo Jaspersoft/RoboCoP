d2133ecfcf14fe6f35c07b5782ab08ad98cadecc
Tom Koptel <tom.koptel@gmail.com>
2014-12-05 12:47:26 +0200

    Updating plugin.Now it depends on 'registerJavaGeneratingTask'

--- plugin/src/main/groovy/com/rain/utils/android/robocop/RobocopPlugin.groovy
+++ plugin/src/main/groovy/com/rain/utils/android/robocop/RobocopPlugin.groovy
@@ -1,23 +1,23 @@
 package com.rain.utils.android.robocop
+
 import com.rain.utils.android.robocop.generator.ContentProviderGenerator
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
-
 /**
  * Created by user on 25.11.2014.
  */
 public class RoboCopPlugin implements Plugin<Project> {
 
+    static final String ROBOCOP_TASK_NAME = 'generateContentProvider'
+
     @Override
     void apply(Project project) {
         def variants = null;
-        def androidExtension = null;
+
         if (project.plugins.findPlugin("com.android.application") || project.plugins.findPlugin("android")) {
-            androidExtension = project.plugins.getPlugin("com.android.application").extension
             variants = "applicationVariants";
         } else if (project.plugins.findPlugin("com.android.library") || project.plugins.findPlugin("android-library")) {
-            androidExtension = project.plugins.getPlugin("com.android.library").extension
             variants = "libraryVariants";
         } else {
             throw new ProjectConfigurationException("The android or android-library plugin must be applied to the project", null)
@@ -27,15 +27,34 @@ public class RoboCopPlugin implements Plugin<Project> {
 
         project.afterEvaluate {
             project.android[variants].all { variant ->
-                configureVariant(project, variant, androidExtension)
+                configureVariant(project, variant)
                 if (variant.testVariant) {
-                    configureVariant(project, variant.testVariant, androidExtension)
+                    configureVariant(project, variant.testVariant)
                 }
             }
         }
     }
 
-    def configureVariant(def project, def variant, def android) {
+    def configureVariant(def project, def variant) {
+        assertPreconditions(project)
+        def outputDir = project.file(new File(project.buildDir, "generated/source/db"))
+        def sourceOutputDir = new File(outputDir, variant.dirName)
+
+        def taskName = "${ROBOCOP_TASK_NAME}${variant.getName()}"
+        def generateTask = project.tasks.create(name: taskName) << {
+            def schemaFile = new File(project.robocop.schemaPath)
+            def schemaPath = schemaFile.getAbsolutePath()
+            def generatedSourcePath = sourceOutputDir.canonicalPath
+            ContentProviderGenerator.generateContentProvider(schemaPath, generatedSourcePath)
+        }
+
+        variant.registerJavaGeneratingTask(generateTask, sourceOutputDir)
+        variant.javaCompile.doFirst {
+            sourceOutputDir.mkdirs()
+        }
+    }
+
+    def assertPreconditions(def project) {
         if (project.robocop.schemaPath == null) {
             project.getLogger().info('RoboCop: Schema path is not defined. Exiting!')
             return;
@@ -44,35 +63,6 @@ public class RoboCopPlugin implements Plugin<Project> {
         if (!schemaFile.exists()) {
             throw new ProjectConfigurationException("Schema file path points to wrong file location", null)
         }
-
-        def robocopOutputDir = project.file(new File(project.buildDir, "generated/source/db"))
-        def robocopOutput = new File(robocopOutputDir, variant.dirName)
-
-        variant.addJavaSourceFoldersToModel(robocopOutput);
-
-//        android.sourceSets[sourceSetName(variant)].java.srcDirs.addAll robocopOutput.path
-//        variant.javaCompile.source = variant.javaCompile.source.filter {
-//            !variant.variantData.extraGeneratedSourceFolders.each { folder ->
-//                folder.path.startsWith(robocopOutput.path)
-//            }
-//        }
-
-        variant.javaCompile.options.compilerArgs += [
-                '-sourcepath', robocopOutput
-        ]
-
-        def schemaPath = schemaFile.getAbsolutePath()
-        def generatedSourcePath = robocopOutput.canonicalPath
-
-        ContentProviderGenerator.generateContentProvider(schemaPath, generatedSourcePath)
-
-        variant.javaCompile.doFirst {
-            robocopOutput.mkdirs()
-        }
-    }
-
-    def sourceSetName(variant) {
-        variant.dirName.split('/').last()
     }
 
 }
--- sample-library/src/main/java/com/example/myapp/MyActivity.java
+++ sample-library/src/main/java/com/example/myapp/MyActivity.java
@@ -2,8 +2,16 @@ package com.example.myapp;
 
 import android.app.Activity;
 
+import com.ifit.android.sdk.model.Fooddetail;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.main)
 public class MyActivity extends Activity {
+
+    @AfterViews
+    final void init() {
+        Fooddetail fooddetail = new Fooddetail();
+    }
 }
